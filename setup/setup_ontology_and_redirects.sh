d2a3ced32a0beb20f2dcb970fbfe857e81177313
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2017-03-28 18:00:03 +0200

    setup more current dump

--- setup/setup_ontology_and_redirects.sh
+++ setup/setup_ontology_and_redirects.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -vv downloads.dbpedia.org/2015-10/core-i18n/en/instance_types_en.ttl.bz2
+wget -vv downloads.dbpedia.org/2016-04/core-i18n/en/instance_types_en.ttl.bz2
 bzip2 -d instance_types_en.ttl.bz2
 mv instance_types_en.ttl ../data/
 
