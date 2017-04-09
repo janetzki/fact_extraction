e79403463e6bb12feb68e4a02af37bced36dbef7
WS2016/2017 <jonathan.janetzki@student.hpi.de>
2017-04-09 09:16:03 +0200

    log progress of syntactic pattern learning

--- pattern_learning/learn_patterns.sh
+++ pattern_learning/learn_patterns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source ../venv/bin/activate
-time nice -n 10 python wikipedia_pattern_extractor.py
-time nice -n 10 python pattern_cleaner.py
+time nice -n 10 python wikipedia_pattern_extractor.py | tee -a results_learning.log
+time nice -n 10 python pattern_cleaner.py | tee -a results_cleaning.log
