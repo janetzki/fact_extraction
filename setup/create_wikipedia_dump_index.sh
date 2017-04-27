af19ab5222fece20954c2927979c10e5b3cbe6a7
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2017-04-27 14:55:07 +0200

    :ambulance: Fix incorrect path

--- setup/create_wikipedia_dump_index.sh
+++ setup/create_wikipedia_dump_index.sh
@@ -2,4 +2,4 @@
 
 BASEDIR=$(dirname "$0")
 
-python $BASEDIR/../wikipedia_connector/dump_connector/wikipedia_dump_index_creator.py
+python $BASEDIR/../wikipedia_connector/wikipedia_dump_index_creator.py
