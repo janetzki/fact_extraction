f52033fb73008a4c5468887d56de328361b7cf1c
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2017-06-02 14:47:34 +0200

    :bug: Close #94: Fix missing start of virtual environment
     #94

--- setup/create_redirected_wikipedia_dump.sh
+++ setup/create_redirected_wikipedia_dump.sh
@@ -2,4 +2,7 @@
 
 BASEDIR=$(dirname "$0")
 
+# start virtual environmet
+source $BASEDIR/../venv/bin/activate
+
 (cd $BASEDIR/.. && python -m data_cleaning.redirects_substitutor)
--- setup/create_wikipedia_dump_index.sh
+++ setup/create_wikipedia_dump_index.sh
@@ -2,4 +2,7 @@
 
 BASEDIR=$(dirname "$0")
 
+# start virtual environmet
+source $BASEDIR/../venv/bin/activate
+
 (cd $BASEDIR/.. && python -m wikipedia_connector.wikipedia_dump_index_creator)
